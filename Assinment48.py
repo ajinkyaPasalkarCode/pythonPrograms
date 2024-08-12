724f5bd0257ad6d607c3f3adacd065e47004bc2e
ajinkyaPasalkarCode <pasalkara02@gmail.com>
2024-08-12 23:31:35 +0530

    Assignment62

--- Assinment48.py
+++ Assinment48.py
@@ -1,3 +1,20 @@
+
+"""
+Assignment - 48 : 5th July'2024
+Print frequency of each character in a given string.
+input : aakanksha
+output : a -> 4
+a -> 4
+k -> 2
+a -> 4
+n -> 1
+k -> 2
+s -> 1
+h -> 1
+a -> 4
+"""
+
+
 class AssignmentNo48:
     def getcharFrequencycount(self,input_string):
         frequency={}                 # Dictionary used key and value
